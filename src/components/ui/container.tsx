8322107eb8bd38159227f8300c5fb7d69e72b265
MohammadAhmerMalick <mohammadahmermalick@gmail.com>
2026-09-19 23:34:11 +0400

    Add xl size variant to button component and create new Container component for layout management

---
+++ src/components/ui/container.tsx
@@ -0,0 +1,36 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
+import { cn } from '@/lib/utils'
+
+const containerVariants = cva('mx-auto', {
+  defaultVariants: {
+    variant: 'default',
+  },
+  variants: {
+    variant: {
+      default: 'mx-auto max-w-8xl px-4 py-16 sm:px-6 md:py-24 lg:px-8',
+    },
+  },
+})
+
+export interface ContainerProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof containerVariants> {}
+
+const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
+  ({ className, children, variant, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(containerVariants({ variant }), className)}
+        {...props}
+      >
+        {children}
+      </div>
+    )
+  }
+)
+
+Container.displayName = 'Container'
+
+export { Container, containerVariants }
